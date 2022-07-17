791e5fa9664b020b988fd1ab1f9d56797e2d9160
imranpradhan <imran.pradhan@elementzerolabs.com>
2022-07-17 12:25:44 +0600

    category button ui

--- src/components/book/Categories.jsx
+++ src/components/book/Categories.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback } from "react";
 import BookService from "../../services/BookService";
 
-const Categories = ({setCategory}) => {
+const Categories = ({ setCategory }) => {
   const [categories, setCategories] = useState([]);
-  const [activeBtn, setActiveBtn] = useState('ALL');
+  const [activeBtn, setActiveBtn] = useState("ALL");
 
   const getAllCategory = useCallback(async () => {
     const {
@@ -15,15 +15,26 @@ const Categories = ({setCategory}) => {
     getAllCategory();
   }, []);
 
-const handleActiveBtn = (val) => {
-  setCategory(val.category)
-  setActiveBtn(val.category)
-}
+  const handleActiveBtn = (val) => {
+    setCategory(val.category);
+    setActiveBtn(val.category);
+  };
   return (
     <div className="flex">
       {categories.map((category) => (
-        <div className="px-[6px] cursor-pointer sm:px-[10px] py-[6px] ml-[4px]" onClick={() => handleActiveBtn(category)}>
-          <p className={`font-black ${activeBtn === category.category ? "text-green-600" : "text-gray-600"}`}>{ category.category}</p>
+        <div
+          className={`px-[6px] hover:bg-sky-400 hover:text-gray-50 cursor-pointer sm:px-[10px] py-[6px] ml-[4px] ${
+            activeBtn === category.category && "bg-sky-400 text-gray-50"
+          }`}
+          onClick={() => handleActiveBtn(category)}
+        >
+          <p
+            className={`font-black ${
+              activeBtn === category.category && "text-gray-50"
+            }`}
+          >
+            {category.category}
+          </p>
         </div>
       ))}
     </div>
